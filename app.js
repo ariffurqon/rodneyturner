5d99784069f5057fa5eedaaa71fc9a43aa9e0e08
ariffurqon <arif.furqon@gmail.com>
2015-11-29 02:40:55 -0800

    added .config and partials

--- app.js
+++ app.js
@@ -8,11 +8,31 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
             controller: 'MainCtrl'
         })
 
+        .when('/services', {
+            templateUrl: 'partials/services.html',
+            controller: 'MainCtrl'
+        })
+
+        .when('/portfolio', {
+            templateUrl: 'partials/portfolio.html',
+            controller: 'MainCtrl'
+        })
+
+        .when('/about', {
+            templateUrl: 'partials/about.html',
+            controller: 'MainCtrl'
+        })
+
+        .when('/contact', {
+            templateUrl: 'partials/contact.html',
+            controller: 'MainCtrl'
+        })
+
         .otherwise({
             redirectTo: '/'
         });
 
-    //
+    //remove the hash and make pretty URLs
     $locationProvider.html5Mode({
         enabled: true,
         requireBase: false
