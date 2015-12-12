a60915f412d70bbd095d68cffe297c062c2e70b2
ariffurqon <arif.furqon@gmail.com>
2015-12-11 17:40:00 -0800

    ready for first deploy

--- server.js
+++ server.js
@@ -16,6 +16,28 @@ mongoose.connect(
   'mongodb://localhost/rodneyturner'
 );
 
+// Processes the form submission.
+app.post('/send', function (req, res) {
+  // Here is an example of how we could send the form data by email
+  var Email = require('emailjs/email');
+  var server = Email.server.connect({
+    host: 'localhost'
+  });
+  server.send({
+    'text': body,
+    'from': req.body.email,
+    'to': arif.furqon@gmail.com,
+    'reply-to': req.body.email,
+    'subject': 'Tell us about your project: ' + req.body.company
+  }, function(error) {
+    if (error) {
+      return res.send({status: 'KO'});
+    } else {
+      return res.send({status: 'OK'});
+    }
+  });
+
+
 // set the static files location 
 app.use(express.static(__dirname + '/public')); 
 
