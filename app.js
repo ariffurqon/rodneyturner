d09c2b357cda3a1626820da886d1a93a72f0865e
ariffurqon <arif.furqon@gmail.com>
2015-12-11 17:51:52 -0800

    updated the contact form

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('rodney', ['ngRoute', 'ajoslin.promise-tracker']);
+var app = angular.module('rodney', ['ngRoute']);
 
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     $routeProvider
